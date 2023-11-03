d12402642ab214349b46c54980f7b597b5c65040
David Sallusti <david.sallusti@alayacare.com>
2023-11-03 11:45:02 -0400

    added extra components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { Checkbox } from 'primereact/checkbox';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 
 export {
-  Checkbox
+  Checkbox,
+  Button,
+  InputText
 };
